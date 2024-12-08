a4cee9530b6e53f4661ee42d05891ac41bd24fee
miruetoto <miruetoto@gmail.com>
2024-12-08 17:59:37 +0900

    .

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='PyEBT',
+        version='0.0.1',
+        description='',
+        url='https://github.com/miruetoto/PyEBT',
+        author='Guebin Choi',
+        author_email='',
+        license='',
+        packages=find_packages(),
+        install_requires=[]
+)
+
+
